b6f29de80a0479a813af51f40e0e9544b462b1ec
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-12-05 16:24:16 +0100

    first commit

---
+++ src/main/java/org/generation/italy/demo/pojo/Pizza.java
@@ -0,0 +1,69 @@
+package org.generation.italy.demo.pojo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Pizza {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column
+	private String name;
+	
+	@Column
+	@Lob
+	private String description;
+	
+	@Column
+	private int price;
+	
+	public Pizza() {}
+	
+	public Pizza(String name, String description, int price) {
+		
+		setName(name);
+		setDescription(description);
+		setPrice(price);
+	}
+
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return "name: " + getName()
+		+ "\nDescription: " + getDescription()
+		+ "\nPrice: " + getPrice();
+	}
+}
