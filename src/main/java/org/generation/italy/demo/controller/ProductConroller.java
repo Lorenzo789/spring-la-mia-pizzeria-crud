57c8623ce8414e204e61da302e58e9995df252c8
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-12-07 18:01:56 +0100

    super bonus to complete

--- src/main/java/org/generation/italy/demo/controller/ProductConroller.java
+++ src/main/java/org/generation/italy/demo/controller/ProductConroller.java
@@ -1,7 +1,9 @@
 package org.generation.italy.demo.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.generation.italy.demo.interf.PriceableInt;
 import org.generation.italy.demo.pojo.Drink;
 import org.generation.italy.demo.pojo.Pizza;
 import org.generation.italy.demo.serv.DrinkService;
@@ -58,4 +60,21 @@ public class ProductConroller {
 		
 		return "product-search";
  	}
+	
+	@GetMapping("/ordered")
+	public String orderByPrice(Model model) {
+		
+		List<PriceableInt> elements = new ArrayList<>(); 
+		
+		List<Pizza> pizzas = pizzaService.findAll();
+		List<Drink> drinks = drinkService.findAll();
+		
+		elements.add((PriceableInt) pizzas);
+		elements.add((PriceableInt) drinks);
+		
+		elements.sort((p1, p2) -> p1.getPrice() - p2.getPrice());
+		model.addAttribute("elements", elements);
+		
+		return "ordered-list";
+	}
 }
